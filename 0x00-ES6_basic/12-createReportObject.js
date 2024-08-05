5a74aed88d126d94c274f538854cc0d20975b562
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-08-05 21:59:25 +0300

    12. Lets create a report object

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,11 @@
+/* eslint-disable linebreak-style */
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments() {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
