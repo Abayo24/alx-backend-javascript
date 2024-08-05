caf98e04c8e381729fe50f3ea5aff9e7f36e4ae7
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-08-05 15:50:01 +0300

    1. Block Scope

--- 0x00-ES6_basic/0-main.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable import/extensions */
-/* eslint-disable linebreak-style */
-import { taskFirst, taskNext } from './0-constants.js';
-
-console.log(`${taskFirst()} ${taskNext()}`);
