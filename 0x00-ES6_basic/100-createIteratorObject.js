f5e60c239bb21b825c92bd2202ed1c003e2fe98f
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-08-05 22:23:16 +0300

    13. Iterating through report objects

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,8 +1,8 @@
 /* eslint-disable linebreak-style */
-export default function createIteratorObject(report) {
-  let employees = [];
-  for (const employee of Object.keys(report.allEmployees)) {
-    employees = employees.concat(report.allEmployees.employee);
+export default function* createIteratorObject(report) {
+  for (const department of Object.values(report.allEmployees)) {
+    for (const employee of department) {
+      yield employee;
+    }
   }
-  return employees;
 }
