de0f23512e1de3819d1c26efcfe9dbe62b9cc18d
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-08-07 12:18:54 +0300

    all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable import/extensions */
-import { uploadPhoto, createUser } from '../utils.js';
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
