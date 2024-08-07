06a0245089ec417edf83659e7607fa4ad6491ecb
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-08-07 12:06:14 +0300

    task3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,13 +2,15 @@
 /* eslint-disable import/extensions */
 import { uploadPhoto, createUser } from '../utils.js';
 
-export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      const [photo, user] = results;
-      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-    })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+export default function handleProfileSignup(status, body, firstName, lastName) {
+  return (
+    Promise.all([uploadPhoto(status, body), createUser(firstName, lastName)])
+      .then((results) => {
+        const [photo, user] = results;
+        console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+      })
+      .catch(() => {
+        console.log('Signup system offline');
+      })
+  );
 }
