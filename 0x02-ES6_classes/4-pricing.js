b76c21d5413b8fa4fceeb019accb37baba5e6d56
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-08-08 17:22:22 +0300

    pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-this-before-super */
+/* eslint-disable no-underscore-dangle */
+import Currency from './3-currency';
+
+export default class Pricing extends Currency {
+  constructor(amount, currency) {
+    super(currency.name, currency.code);
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(pricingCurrency) {
+    this._currency = pricingCurrency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(pricingAmount) {
+    this._amount = pricingAmount;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
