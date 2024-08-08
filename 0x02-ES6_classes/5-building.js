73ec1a463548fde21cc35a31d21efd5bdcf58b2e
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-08-08 20:33:32 +0300

    abstract class

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-underscore-dangle */
+export default class Building {
+  constructor(sqft) {
+    if (this.evacuationWarningMessage() === 'undefined') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
