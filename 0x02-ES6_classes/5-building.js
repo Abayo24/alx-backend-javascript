e5b7ed3c6d3cde2359bfdeef07ad1edf94285b4c
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-08-08 21:06:33 +0300

    nest if statement

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,8 +2,10 @@
 /* eslint-disable no-underscore-dangle */
 export default class Building {
   constructor(sqft) {
-    if (this.evacuationWarningMessage() === 'undefined') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (this.constructor !== Building) {
+      if (this.evacuationWarningMessage() === 'undefined') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
     }
     this._sqft = sqft;
   }
