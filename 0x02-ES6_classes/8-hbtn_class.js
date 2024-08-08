0f8504a133be88e2ffaa2d1433532cbfe0f4277e
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-08-08 22:29:38 +0300

    primitive symbol

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-underscore-dangle */
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](value) {
+    if (value === 'number') {
+      return this._size;
+    }
+    if (value === 'string') {
+      return this._location;
+    }
+    return value;
+  }
+}
