982e4375352cf0eef549cf208bace35b744e3d78
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-08-12 18:29:04 +0300

    eslint

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,7 +2,6 @@ export default function getListStudentIds(arrayOfObjects) {
   if (!Array.isArray(arrayOfObjects)) {
     return [];
   }
-  const arrayOfIds = arrayOfObjects.map(student => student.id);
+  const arrayOfIds = arrayOfObjects.map((student) => student.id);
   return arrayOfIds;
-  
 }
