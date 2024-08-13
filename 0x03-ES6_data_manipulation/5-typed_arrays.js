031ebe5454f31e404540ad2df1ea7e30cdd86792
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-08-13 12:49:45 +0300

    typed array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,4 @@
-export default function createInt8TypedArray(length, position) {
+export default function createInt8TypedArray(length, position, values) {
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
   if (position < 0 || position > length) {
