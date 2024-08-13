8693257f2fc2a835e71f345502faa21badc8a4c8
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-08-13 12:54:49 +0300

    typed array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -4,5 +4,6 @@ export default function createInt8TypedArray(length, position, values) {
   if (position < 0 || position > length) {
     throw new Error('Position outside range');
   }
+  view.setInt8();
   return view;
 }
