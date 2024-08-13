f5d18f561bd3174b885c3da749dcca85b67e0140
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-08-13 09:48:52 +0300

    Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default function cleanSet(set, startString) {
+  if (!startString) {
+    return '';
+  }
+  const result = [];
+  for (const elem of set) {
+    if (elem.startsWith(startString)) {
+      result.push(elem.slice(startString.length));
+    }
+  }
+  return result.join('-');
+}
