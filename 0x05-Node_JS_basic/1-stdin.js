02483f857140e6b9fc3634fdc832fe366b3bba32
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-09-30 14:22:54 +0300

    using process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,17 @@
+const displayMessage = require('./0-console');
+const readline = require('readline');
+
+const read = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+displayMessage('Welcome to Holberton School, what is your name?')
+
+read.on('line', (input) => {
+  console.log(`Your name is: ${input}`);
+});
+
+read.on('close', () => {
+  console.log('This important software is now closing')
+});
