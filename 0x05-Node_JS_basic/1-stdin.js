eb046e86e089a391147bad46558c3fc1ce4c469c
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-10-01 10:52:24 +0300

    eslint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
-const displayMessage = require('./0-console');
 const readline = require('readline');
+const displayMessage = require('./0-console');
 
 const read = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-displayMessage('Welcome to Holberton School, what is your name?')
+displayMessage('Welcome to Holberton School, what is your name?');
 
 read.on('line', (input) => {
   console.log(`Your name is: ${input}`);
