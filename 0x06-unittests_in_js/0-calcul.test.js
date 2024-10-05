8c447257d5891f2769fdeb11d5316b29519d4523
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-10-05 18:23:49 +0300

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -17,4 +17,7 @@ describe('calculateNumber', () => {
     it('should return 6 when 1.5 and 3.7 are passed', () => {
         assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
+    it('should return 0 when 0.1 and 0.3 are passed', () => {
+        assert.strictEqual(calculateNumber(0.1, 0.3), 0);
+    });
 });
