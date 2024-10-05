713612803f7ba842c9f91eae5482eb4dbaa5760c
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-10-05 19:54:26 +0300

    hooks

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,30 @@
+const sinon = require('sinon');
+const assert = require('assert');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleSpy.restore();
+  });
+
+  it('should log the correct total for 100 and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    assert(consoleSpy.calledOnce);
+    assert(consoleSpy.calledWith('The total is: 120'));
+  });
+
+  it('should log the correct total for 10 and 10', () => {
+    sendPaymentRequestToApi(10, 10);
+
+    assert(consoleSpy.calledOnce);
+    assert(consoleSpy.calledWith('The total is: 20'));
+  });
+});
