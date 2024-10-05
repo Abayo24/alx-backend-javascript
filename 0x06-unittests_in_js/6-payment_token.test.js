7f7c959db39704e93bd874920216cb8daaf7ba5d
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-10-05 20:13:36 +0300

    Async tests with done

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,25 +2,21 @@ const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  it('should return a successful response when success is true', (done) => {
-    getPaymentTokenFromAPI(true)
-      .then((response) => {
-        expect(response).to.deep.equal({ data: 'Successful response from the API' });
-        done();
-      })
-      .catch((error) => {
-        done(error);
-      });
-  });
+    it('should return a successful response when success is true', (done) => {
+        getPaymentTokenFromAPI(true)
+            .then((response) => {
+                expect(response).to.deep.equal({ data: 'Successful response from the API' });
+                done();
+            });
+    });
 
-  it('should do nothing when success is false', (done) => {
-    getPaymentTokenFromAPI(false)
-      .then((response) => {
-        expect(response).to.be.undefined;
-        done();
-      })
-      .catch((error) => {
-        done(error);
-      });
-  });
+    it('should do nothing when success is false', (done) => {
+        const result = getPaymentTokenFromAPI(false);
+        expect(result).to.be.an.instanceof(Promise);
+        result.then(() => {
+            done(new Error('Expected method to do nothing.'));
+        }).catch(() => {
+            done();
+        });
+    });
 });
