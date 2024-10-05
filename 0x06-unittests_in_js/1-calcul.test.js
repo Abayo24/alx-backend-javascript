72d371d03cb5c16f5717cecf42d995f00ebfbeae
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-10-05 19:03:10 +0300

    Combining descriptions

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -2,47 +2,44 @@ const assert = require('assert');
 const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', () => {
+  // Test suite for SUM
   describe('SUM', () => {
-    it('should return 6 when SUM and 1.4 and 4.5 are passed', () => {
+    it('should return 6 for calculateNumber(SUM, 1.4, 4.5)', () => {
       assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
     });
 
-    it('should return 5 when SUM and 1.2 and 3.3 are passed', () => {
+    it('should return 5 for calculateNumber(SUM, 1.2, 3.3)', () => {
       assert.strictEqual(calculateNumber('SUM', 1.2, 3.3), 5);
     });
   });
 
+  // Test suite for SUBTRACT
   describe('SUBTRACT', () => {
-    it('should return -4 when SUBTRACT and 1.4 and 4.5 are passed', () => {
+    it('should return -4 for calculateNumber(SUBTRACT, 1.4, 4.5)', () => {
       assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
     });
 
-    it('should return 0 when SUBTRACT and 3.5 and 3.5 are passed', () => {
+    it('should return 0 for calculateNumber(SUBTRACT, 3.5, 3.5)', () => {
       assert.strictEqual(calculateNumber('SUBTRACT', 3.5, 3.5), 0);
     });
   });
 
+  // Test suite for DIVIDE
   describe('DIVIDE', () => {
-    it('should return 0.2 when DIVIDE and 1.4 and 4.5 are passed', () => {
+    it('should return 0.2 for calculateNumber(DIVIDE, 1.4, 4.5)', () => {
       assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
     });
 
-    it('should return Error when DIVIDE and 1.4 and 0 are passed', () => {
+    it('should return "Error" for calculateNumber(DIVIDE, 1.4, 0)', () => {
       assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
     });
 
-    it('should return 1 when DIVIDE and 2.5 and 2 are passed', () => {
+    it('should return 1 for calculateNumber(DIVIDE, 2.5, 2)', () => {
       assert.strictEqual(calculateNumber('DIVIDE', 2.5, 2), 1);
     });
-  });
 
-  describe('Edge Cases', () => {
-    it('should return Error when DIVIDE and 0 and 0 are passed', () => {
+    it('should return "Error" for calculateNumber(DIVIDE, 0, 0)', () => {
       assert.strictEqual(calculateNumber('DIVIDE', 0, 0), 'Error');
-    });
-
-    it('should return -1 when SUBTRACT and -1.5 and 0.5 are passed', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', -1.5, 0.5), -2);
     });
   });
 });
