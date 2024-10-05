52f31cdf45db45bbcf735a870f986e9a44c2776d
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-10-05 20:05:15 +0300

    Async tests with done

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -2,8 +2,6 @@ function getPaymentTokenFromAPI(success) {
     return new Promise((resolve, reject) => {
         if (success) {
           resolve({ data: 'Successful response from the API' });
-        } else {
-          reject(new Error('Failed to get payment token'))
         }
       });
 }
